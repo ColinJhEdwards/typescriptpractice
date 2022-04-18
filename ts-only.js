a98f1820b7cdcdce5eacd1be90755892cef4b8bc
Colin Edwards <ColinJhEdwards@gmail.com>
2022-04-17 19:50:15 -0700

    additional comments

--- ts-only.js
+++ ts-only.js
@@ -1,11 +1,11 @@
-//  the ! means that the variable will never return null, we are sure of this as a developer we checked the code
+// This is the compiled ts file which is now js, though previous comments removed
 var input1 = document.querySelector(".one");
 var input2 = document.querySelector(".two");
 var btn = document.querySelector(".button");
-// instead of num1 / num2, we assign them a specific type num1: number
+
 function add(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 btn.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
+  console.log(add(+input1.value, +input2.value));
 });
--- ts-only.ts
+++ ts-only.ts
@@ -9,8 +9,10 @@ function add(num1: number, num2: number) {
 }
 
 btn.addEventListener("click", () => {
+  // an error would be thrown here due to input1.value and input2.value being a string, because of typescript we know
+  // to change the input value to a number
   console.log(add(+input1.value, +input2.value));
 });
 
 // compile the typescript file into javascript by running "tsc ts-only.ts"
-// this will create a javascript file but also return any errors
+// this will create a javascript file but also return errors if any are present.
